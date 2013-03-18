415695b005c3957e939a0ac91747e8d526a0849b
Daniel Abrahamsson <hamsson@gmail.com>
2013-03-18 10:15:12 +0100

    Define methods for searching

--- gecode.erl
+++ gecode.erl
@@ -1,33 +1,100 @@
 %% Experimenting with implementing Gecode in Erlang.
 
 -module(gecode).
--export([problem1/0]).
+-export([problem1/0, model/4, search_all/1, search_all/2, search_next/1, search_next/2]).
 
 -record(space, {vars=[], propagators=[], branchers=[], search_method=dfs, status=unsolved, subspaces, parent}).
 -record(propagator, {implementation, vars}).
 -record(var, {type, id, domain}).
 -record(brancher, {vars, var_strategy, val_strategy}).
 
-solve(Vars, Propagators, Branchers, SearchMethod) ->
-  Space = #space{vars=Vars, propagators=Propagators, branchers=Branchers, search_method=SearchMethod, status=open},
-  SearchFun = search_method_for(SearchMethod),
-  SearchFun([Space]).
 
-search_method_for(dfs) -> fun dfs/1.
+% Returns a new space describing the given model.
+% Vars should be a list of variables in the model.
+% Propagators should be a list of propagators defining the constraints of the model.
+% Branchers should be a list of branchers describing how to create the search tree.
+% SearchMethod should be either the atom dfs, or a tuple {SearchFun, SearchParams}.
+% A search function is a function taking two parameters:
+% a list of spaces to search and an arbitrary parameter needed to continue
+% a previously started search.
+% The search function should stop at the first successful leaf it encounters.
+% All search functions must return the following tuple:
+% {Result, Leaves, NextSpaces, NextParams}.
+% Result should be either of the atoms solved or failed.
+% Leaves should contain all the leaves needed for building up the search tree.
+% NextSpaces should be a list of spaces to search next to find the next solution.
+% NextParams should contain the parameters needed for continuing the search.
+model(Vars, Propagators, Branchers, SearchMethod) ->
+  #space{vars=Vars, propagators=Propagators, branchers=Branchers, search_method=SearchMethod, status=open}.
 
-dfs([]) ->
-  failed;
+% Finds all solutions in the given space's subtree.
+% Returns a tuple {Solutions, Leaves}, where Solutions is a list of spaces
+% representing all solutions to the problem, and Leaves is a list of all
+% leaves in the search tree. 
+search_all(Space=#space{search_method=SearchMethod}) ->
+  {_, StartParams} = search_data_for(SearchMethod),
+  search_all([Space], StartParams).
 
-dfs([Space|Ss]) ->
+% Finds all solutions for the given spaces.
+% StartParams should contain parameters needed for continuing any  previous search.
+search_all(Spaces=[S|_], StartParams) ->
+  #space{search_method=SearchMethod} = S,
+  {SearchFun, _} = search_data_for(SearchMethod),
+  search_all(Spaces, SearchFun, StartParams, [], []).
+
+search_all([], _, _, Solutions, LeafNodes) ->
+  {Solutions, LeafNodes};
+
+search_all(Spaces, SearchFun, Params, Solutions, _LeafNodes) ->
+  {Res, NewLeafNodes, NextSpaces, NextParams} = search_next(Spaces, SearchFun, Params),
+  case Res of
+    solved ->
+      [Solution|_] = NewLeafNodes,
+      search_all(NextSpaces, SearchFun, NextParams, [Solution|Solutions], NewLeafNodes);
+    failed ->
+      {Solutions, NewLeafNodes}
+  end.
+
+% Searches for the next solution in the given space.
+search_next(Space=#space{search_method=SearchMethod}) ->
+  {SearchFun, StartParams} = search_data_for(SearchMethod),
+  search_next([Space], SearchFun, StartParams).
+
+% Searches for the next solution for the given list of spaces.
+search_next(Spaces=[S|_], StartParams) ->
+  #space{search_method=SearchMethod} = S,
+  {SearchFun, _} = search_data_for(SearchMethod),
+  search_next(Spaces, SearchFun, StartParams).
+
+search_next(Spaces, SearchFun, Params) ->
+  SearchFun(Spaces, Params).
+
+% Returns a tuple {SearchFun, StartParams}. SearchFun can be used to
+% search a space, and takes two parameters: The spaces to search, and
+% any parameters to pass on to the search engine in the form of a list.
+% StartParams is a list of parameters to pass into the search function
+% at the first invocation of the search.
+search_data_for(dfs) ->
+  { fun dfs/2, [] };
+
+search_data_for({Fun, _StartParams}=ToUse) when is_function(Fun) ->
+  ToUse.
+
+% Performs depth first search for a solution.
+dfs([], Leaves) ->
+  {failed, Leaves, [], []};
+
+dfs([Space|Ss], Leaves) ->
   Space1 = propagate(Space),
   case evaluate(Space1) of
     unsolved ->
       Subspaces = branch_on(Space1),
-      dfs(Subspaces ++ Ss);
+      dfs(Subspaces ++ Ss, Leaves);
     failed ->
-      dfs(Ss);
+      dfs(Ss, [Space1#space{status=failed}|Leaves]);
     solved ->
-      {solved, Space1#space{status=solved}, Ss}
+      NewLeaves = [Space1#space{status=solved}|Leaves],
+      {solved, NewLeaves, Ss, NewLeaves}
   end.
 
 propagate(Space = #space{vars=Vars, propagators=Propagators}) ->
@@ -91,7 +158,7 @@ select_brancher(Branchers, Vars) ->
 
 select_brancher1([B|Bs], VarIds) ->
   #brancher{vars=BranchVarIds}= B,
-  case lists:any(fun(X) -> lists:member(X, VarIds) end, BranchVarIds) of 
+  case lists:any(fun(X) -> lists:member(X, VarIds) end, BranchVarIds) of
     true -> B;
     false ->  select_brancher1(Bs, VarIds)
   end.
@@ -113,7 +180,7 @@ select_branch_variable(Vars, int_var_size_min) ->
 % Branches on the smallest value in the domain of the selected variable.
 branch_on(Space, Var=#var{domain=[Min|Rest]} , RestOfVars, int_val_min) ->
   MinSpace = Space#space{vars=[Var#var{domain=[Min]}|RestOfVars], parent=Space},
-  RestSpace = Space#space{vars=[Var#var{domain=[Rest]}|RestOfVars], parent=Space},
+  RestSpace = Space#space{vars=[Var#var{domain=Rest}|RestOfVars], parent=Space},
   [MinSpace, RestSpace];
 
 branch_on(Space, Var, RestOfVars, Fun) when is_function(Fun) ->
@@ -145,15 +212,13 @@ branch(Vars=[_|_], VarStrategy, ValStrategy) ->
 int_var(Id, Min, Max) when is_atom(Id), is_integer(Min), is_integer(Max), Max >= Min ->
   {var, integer, Id, lists:seq(Min, Max)}.
 
-
-
 problem1() ->
   Vars = [S=int_var(s, 0, 9)],
 
   Propagators = [ne(S, 0)],
-  
+
   Branchers = [branch(Vars, int_var_size_min, int_val_min)],
 
   SearchMethod = dfs,
 
-  solve(Vars, Propagators, Branchers, SearchMethod).
+  model(Vars, Propagators, Branchers, SearchMethod).
